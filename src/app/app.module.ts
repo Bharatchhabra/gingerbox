11ac63ce9f7781f3d2d259855d8add4ddd4a8d5c
bharatchhabra <you@example.com>
2021-05-05 19:09:18 +0530

    commit sec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,13 @@ import { ConformRegFormComponent } from './pages/conform-reg-form/conform-reg-fo
 import { FillingFormComponent } from './pages/filling-form/filling-form.component';
 import { AddinformationComponent } from './pages/addinformation/addinformation.component';
 import { AddmoreinfomationaboutComponent } from './pages/addmoreinfomationabout/addmoreinfomationabout.component';
-import { IdProofComponent } from './id-proof/id-proof.component';
+import { IdProofComponent } from './pages/id-proof/id-proof.component';
+import { AddressProofComponent } from './pages/address-proof/address-proof.component';
+import { HouseDetailsComponent } from './pages/house-details/house-details.component';
+import { RoomsDetailsComponent } from './pages/rooms-details/rooms-details.component';
+import { ConformationDetailsComponent } from './pages/conformation-details/conformation-details.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { LastconformationpageComponent } from './pages/lastconformationpage/lastconformationpage.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +25,14 @@ import { IdProofComponent } from './id-proof/id-proof.component';
     FillingFormComponent,
     AddinformationComponent,
     AddmoreinfomationaboutComponent,
-    IdProofComponent
+    IdProofComponent,
+    AddressProofComponent,
+    HouseDetailsComponent,
+    RoomsDetailsComponent,
+    ConformationDetailsComponent,
+    ErrorComponent,
+    LastconformationpageComponent
+
   ],
   imports: [
     BrowserModule,
